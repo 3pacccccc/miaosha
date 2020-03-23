7c2688fede9ee095e99a78f6994668619d4de867
3pacccccc <351489917@qq.com>
2020-03-24 00:11:58 +0800

    5-1

--- src/main/java/com/imooc/miaosha_1/controller/GoodsController.java
+++ src/main/java/com/imooc/miaosha_1/controller/GoodsController.java
@@ -2,13 +2,24 @@ package com.imooc.miaosha_1.controller;
 
 
 import com.imooc.miaosha_1.domain.MiaoshaUser;
+import com.imooc.miaosha_1.redis.GoodsKey;
+import com.imooc.miaosha_1.redis.RedisService;
 import com.imooc.miaosha_1.service.GoodsService;
 import com.imooc.miaosha_1.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping("/goods")
@@ -17,12 +28,79 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping("to_list")
-    public String list(Model model, MiaoshaUser user) {
+    @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
+
+    @RequestMapping(value = "to_list", produces = "text/html")
+    @ResponseBody
+    public String list(HttpServletRequest request, HttpServletResponse response, Model model, MiaoshaUser user) {
+
         model.addAttribute("user", user);
-        model.addAttribute("goodsList", goodsService.listGoodsVo());
-        return "goods_list";
+        // 取缓存
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        // 如果没取到redis里面的缓存，就自己生成数据
+        List<GoodsVo> goodsVoList = goodsService.listGoodsVo();
+        model.addAttribute("goodsList", goodsVoList);
+        SpringWebContext ctx = new SpringWebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap(), applicationContext);
+        // 手动渲染
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            // 存入redis缓存
+            redisService.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
+
+    @RequestMapping(value = "to_detail2/{goodsId}", produces = "text/html")
+    @ResponseBody
+    public String detail2(HttpServletRequest request, HttpServletResponse response, Model model,
+                          MiaoshaUser user, @PathVariable("goodsId") long goodsId) {
+        model.addAttribute("user", user);
+        String html = redisService.get(GoodsKey.getGoodsDetail, "" + goodsId, String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        // 手动渲染
+        GoodsVo goods = goodsService.getGoodsByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+        if (now < startAt) {//秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int) ((startAt - now) / 1000);
+        } else if (now > endAt) {//秒杀已经结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        } else {//秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+
+        model.addAttribute("miaoshaStatus", miaoshaStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+
+        SpringWebContext ctx = new SpringWebContext(request, response,
+                request.getServletContext(), request.getLocale(), model.asMap(), applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_detail", ctx);
+        if (!org.apache.commons.lang3.StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsDetail, "" + goodsId, html);
+        }
+    }
+
 
     @RequestMapping("/to_detail/{goodsId}")
     public String detail(Model model, MiaoshaUser user, @PathVariable("goodsId") long goodsId) {
